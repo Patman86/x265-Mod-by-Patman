4ca1c35176c1eca62268993eb7503e5bb6c51be3
Patman86 <54327252+Patman86@users.noreply.github.com>
2021-12-14 18:44:52 +0100

    add StopReader

--- source/abrEncApp.cpp
+++ source/abrEncApp.cpp
@@ -837,9 +837,11 @@ ret:
             m_cliopt.output->closeFile(largest_pts, second_largest_pts);
 
             if (b_ctrl_c)
+			{
                 general_log(m_param, NULL, X265_LOG_INFO, "aborted at input frame %d, output frame %d in %s\n",
                     m_cliopt.seek + inFrameCount, stats.encodedPictureCount, profileName);
-
+				m_input->stopReader();
+            }
             api->param_free(m_param);
 
             X265_FREE(errorBuf);
@@ -1074,7 +1076,7 @@ ret:
             }
 
             x265_picture* dest = m_parentEnc->m_parent->m_inputPicBuffer[m_id][writeIdx];
-            if (m_input->readPicture(*src))
+            if (m_input->readPicture(*src) && !b_ctrl_c)
             {
                 dest->poc = src->poc;
                 dest->pts = src->pts;
