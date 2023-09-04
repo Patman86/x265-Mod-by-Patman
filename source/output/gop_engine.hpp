0358f0530c9d68ca92a29e4fd3559c67f6914aa5
Patman86 <suckzpsych0@gmail.com>
2023-09-04 23:43:25 +0200

    fix compiling issue under GCC

--- source/output/gop_engine.hpp
+++ source/output/gop_engine.hpp
@@ -1,20 +1,20 @@
 /*****************************************************************************
  * MIT License
- * 
+ *
  * Copyright (c) 2022 Xinyue Lu
  *
  * Authors: Xinyue Lu <i@7086.in>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,9 +31,11 @@
 #include <sstream>
 #include <iomanip>
 
-#ifdef _MSC_VER
+#if _MSC_VER
 #include <windows.h>
 #define sleep(x) Sleep((x) * 1000)
+#else
+#include <unistd.h>
 #endif
 #define TIME_WAIT 30
 
