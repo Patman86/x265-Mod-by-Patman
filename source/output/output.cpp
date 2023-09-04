855d8544707c06181efc33d9287f7b70c3e9bf97
Patman86 <suckzpsych0@gmail.com>
2023-09-04 23:03:40 +0200

    "GOP" output overhaul

--- source/output/output.cpp
+++ source/output/output.cpp
@@ -42,9 +42,9 @@ ReconFile* ReconFile::open(const char *fname, int width, int height, uint32_t bi
 
 OutputFile* OutputFile::open(const char *fname, InputFileInfo& inputInfo)
 {
-	const char * s = strrchr(fname, '.');
+    const char * s = strrchr(fname, '.');
 
-	if (s && !strcmp(s, ".gop"))
+    if (s && !strncmp(s, ".gop", 4))
         return new GOPOutput(fname, inputInfo);
 
     return new RAWOutput(fname, inputInfo);
